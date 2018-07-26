46aef383b16fdd9c2c888621b3af30f866728272
Md Sadique <msadique265@yahoo.com>
2018-07-26 15:17:55 +0530

    Initial commit

---
+++ My_Logger/__init__.py
@@ -0,0 +1,1 @@
+name = "My_Logger"
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='My_Logger',
+    version='0.0.1',
+    description='Customised logging library',
+    license='MIT',
+    packages=['My_Logger'],
+    author='Md Sadique',
+    author_email='msadique265@yahoo.com',
+    keywords=['python logging'],
+    url='https://github.com/msadique265/Modules/My_Logger'
+)
